592382a0220b1e91a2ce4d63cd5361a9b001620c
주희 <dlwngml21@gmail.com>
2022-07-15 15:23:44 +0900

    join page 수정 - 이경은

--- join.php
+++ join.php
@@ -4,12 +4,19 @@
     <meta charset="utf-8">
     <title>회원 가입</title>
     <link rel="stylesheet" href="style_contents.css" type="text/css">
+    <script type="text/javascript">
+    function maxLengthCheck(object){
+      if (object.value.length > object.maxLength){
+    object.value = object.value.slice(0, object.maxLength);
+  }
+}
+    </script>
   </head>
   <body>
     <iframe src="head.php" width="100%" frameborder="0"></iframe>
-    <div class="main_contents">
+    <div class="contents">
       <form class="mfrom" action="join_ok.php" method="post">
-        <table>
+        <table width="550">
         <tr>
           <th colspan="2" style="background-color:#323232">
             <font style="color:white; font-size:150%">회 원 가 입</font></th>
@@ -21,29 +28,28 @@
           </td>
         </tr>
         <tr>
-          <th>비밀번호</th>
+          <th>*비밀번호</th>
           <td>
             <input type="password" name="user_pw1" size="20" maxlength="20">
             &nbsp;&nbsp;&nbsp;<font style="color:red;">6~20(영문/숫자/특수문자)</font>
           </td>
         </tr>
         <tr>
-          <th>비밀번호 확인</th>
+          <th>*비밀번호 확인</th>
           <td>
-            <input type="password" name="user_pw1" size="20" maxlength="20">
+            <input type="password" name="user_pw2" size="20" maxlength="20">
           </td>
         </tr>
         <tr>
-          <th>이름</th>
+          <th>*이름</th>
           <td>
             <input type="text" name="name" size="15" maxlength="15">
           </td>
         </tr>
         <tr>
-          <th>주민등록번호</th>
+          <th>*주민등록번호</th>
           <td>
-            <input type="number" name="id_num1" size="8" maxlength="6" >-
-            <input type="number" name="id_num2" size="8" maxlength="7">
+            <input type="number" name="id_num" size="15" maxlength="13" oninput="maxLengthCheck(this)"> &nbsp;&nbsp;&nbsp; <font style="color:red;">- 는 제외하고 숫자만 입력</font>
           </td>
         </tr>
         <tr>
@@ -55,7 +61,7 @@
         <tr>
           <th>전화번호</th>
           <td>
-            <input type="number" name="tel" size="20" maxlength="11">
+            <input type="number" name="tell" size="20" maxlength="11" oninput="maxLengthCheck(this)"> <font style="color:red;">- 는 제외하고 숫자만 입력</font>
           </td>
         </tr>
         <tr>
@@ -64,8 +70,8 @@
         </tr>
         </table>
         <p>
-          <font size=2>   *는 필수 입력 항목입니다. </font><br><br>
-            <input type="button" value="등록" class="btn_default btn_gray" onclick="ck()">
+          <font size="2">   *는 필수 입력 항목입니다. </font><br><br>
+            <input type="submit" value="등록" class="btn_default btn_gray">
             <input type="reset" value="삭제" class="btn_default btn_gray">
           </p>
       </form>
